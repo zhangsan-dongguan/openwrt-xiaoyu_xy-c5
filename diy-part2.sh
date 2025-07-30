25da7fcbe6c809e5dc9d6b3e7ab01b81006f32d4
zhangsan-dongguan <76426725+zhangsan-dongguan@users.noreply.github.com>
2025-07-30 09:09:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.0.3/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.0.3/192.168.0.3/g' package/base-files/files/bin/config_generate
 
 # make defconfig
 sed -i 's/^[ \t]*//g' ./.config
